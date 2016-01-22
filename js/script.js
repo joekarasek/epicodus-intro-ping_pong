fa88af23fe475aa0c6f42a867b7a0cc38c89b6eb
Joseph Karasek <joseph.karasek@gmail.com>
2016-01-22 08:28:13 -0800

    add return pongping ability to toPingPong

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@
 //++++++++++++++++++++++++++++++++
 
 var toPingPong = function(inputNumber) {
-  if (inputNumber % 5 === 0) {
+  if (inputNumber % 15 === 0) {
+    return "pingpong";
+  } else if (inputNumber % 5 === 0) {
     return "pong";
   } else if (inputNumber % 3 === 0) {
     return "ping";
