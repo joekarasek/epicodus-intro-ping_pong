42fce5ae42b07d85cb277f599c229f884ba6b1eb
Joseph Karasek <joseph.karasek@gmail.com>
2016-01-22 08:20:23 -0800

    add spec for toPingPong, return ping on inputs divisible by 3

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,11 @@
 describe('toPingPong', function() {
-  it('it will return a number as a string if its not divisble b 3 or 5', function() {
+  it('it will return a number as a string if input is not divisble by 3 or 5', function() {
     expect(toPingPong(4)).to.equal('4');
   });
+  it('it will return the string "ping" if input is divisble by 3', function() {
+    expect(toPingPong(6)).to.equal('ping');
+  });
+  // it('it will return a number as a string if its not divisble b 3 or 5', function() {
+  //   expect(toPingPong(4)).to.equal('4');
+  // });
 });
