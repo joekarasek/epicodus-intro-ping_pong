cc8f23ee98fc735f07a3ad59fa09111b98d6b4a9
Joseph Karasek <joseph.karasek@gmail.com>
2016-01-22 08:24:07 -0800

    add specs for div by 5 and div by 3 and 5 to toPingPong

--- spec/specs.js
+++ spec/specs.js
@@ -5,7 +5,10 @@ describe('toPingPong', function() {
   it('it will return the string "ping" if input is divisble by 3', function() {
     expect(toPingPong(6)).to.equal('ping');
   });
-  // it('it will return a number as a string if its not divisble b 3 or 5', function() {
-  //   expect(toPingPong(4)).to.equal('4');
-  // });
+  it('it will return the string "pong" if input is divisble by 5', function() {
+    expect(toPingPong(5)).to.equal('pong');
+  });
+  it('it will return the string "pingpong" if input is divisble by both 3 and 5', function() {
+    expect(toPingPong(15)).to.equal('pingpong');
+  });
 });
